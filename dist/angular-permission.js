97acf3e0a7ed43e2392b6d1dcb2d0d28e3cacf06
sergey <sergey.tarasov@muranosoft.com>
2016-06-28 11:51:06 +0300

    Remove state change handling

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -8,65 +8,6 @@
  */
 
 (function () {
-  'use strict';
-
-  angular.module('permission', ['ui.router'])
-    .run(['$rootScope', 'Permission', '$state', function ($rootScope, Permission, $state) {
-      $rootScope.$on('$stateChangeStart',
-      function (event, toState, toParams, fromState, fromParams, options) {
-        // If there are permissions set then prevent default and attempt to authorize
-        var permissions;
-        if (toState.data && toState.data.permissions) {
-          permissions = toState.data.permissions;
-        } else if (toState.permissions) {
-          /**
-          * This way of defining permissions will be depracated in v1. Should use
-          * `data` key instead
-          */
-          console.log('Deprecation Warning: permissions should be set inside the `data` key ');
-          console.log('Setting permissions for a state outside `data` will be depracated in' +
-            ' version 1');
-          permissions = toState.permissions;
-        }
-
-        if (permissions) {
-          event.preventDefault();
-
-          Permission.authorize(permissions, toParams).then(function () {
-            // If authorized, use call state.go without triggering the event.
-            // Then trigger $stateChangeSuccess manually to resume the rest of the process
-            // Note: This is a pseudo-hacky fix which should be fixed in future ui-router versions
-            if (!$rootScope.$broadcast('$stateChangeStart', toState.name, toParams, fromState.name, fromParams).defaultPrevented) {
-              $rootScope.$broadcast('$stateChangePermissionAccepted', toState, toParams);
-			  options = options || {};
-
-              options.notify = false;
-
-              $state.go(toState.name, toParams, options).then(function() {
-                $rootScope
-                  .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-              });
-            }
-          }, function () {
-            if (!$rootScope.$broadcast('$stateChangeStart', toState.name, toParams, fromState.name, fromParams).defaultPrevented) {
-              $rootScope.$broadcast('$stateChangePermissionDenied', toState, toParams);
-
-              // If not authorized, redirect to wherever the route has defined, if defined at all
-              var redirectTo = permissions.redirectTo;
-              if (redirectTo) {
-                $state.go(redirectTo, toParams, {notify: false}).then(function() {
-                  $rootScope
-                    .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-                });
-              }
-            }
-          });
-        }
-      });
-    }]);
-}());
-
-(function () {
     'use strict';
 
     angular.module('permission')
